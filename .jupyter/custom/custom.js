8768698a7dbd0e479396047309a1e489c28ce18e
hysts <hysts@users.noreply.github.com>
2017-02-05 17:13:04 +0900

    Use vim keybinding with jupyter notebook

--- .jupyter/custom/custom.js
+++ .jupyter/custom/custom.js
@@ -1,9 +1,12 @@
 define([
     'base/js/namespace',
     'base/js/events',
-    'notebook/js/cell'
+    'notebook/js/cell',
+    'jquery',
+    'codemirror/keymap/vim',
+    'codemirror/addon/dialog/dialog'
     ],
-    function(Jupyter, events, cell) {
+    function(Jupyter, events, cell, $) {
         events.on("app_initialized.NotebookApp",
             function () {
                 // Show line numbers in cells
@@ -36,6 +39,27 @@ define([
 
                 // Hide toolbar
                 //Jupyter.toolbar.actions.call('jupyter-notebook:toggle-toolbar')
+
+                Jupyter.keyboard_manager.edit_shortcuts.remove_shortcut('esc');
+                cell.Cell.options_default.cm_config.vimMode = true;
+
+                // codemirror dialog for cmdline and search
+                $('head').append('<link rel="stylesheet" type="text/css" ' +
+                    'href="/static/components/codemirror/addon/dialog/dialog.css">')
+                $('head').append('<style type="text/css">' +
+                    '.CodeMirror-dialog {opacity: 0.9 !important;}</style>');
+
+                // avoid ipython command mode while in codemirror dialog
+                var bind_events = cell.Cell.prototype.bind_events;
+                cell.Cell.prototype.bind_events = function () {
+                    bind_events.apply(this);
+                    if (!this.code_mirror) return;
+                    var that = this;
+                    this.code_mirror.on('blur', function() {
+                        if ($('.CodeMirror-dialog').length > 0)
+                            that.events.trigger('edit_mode.Cell', {cell: that});
+                    });
+                }
 
             }
         );
